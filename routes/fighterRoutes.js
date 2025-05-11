ebbb2d7c60bda5e84cd221a39b518c526cbd1099
Ihor Artomov <igora2406@gmail.com>
2025-05-11 20:20:36 +0300

    Fighters API

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -9,5 +9,89 @@ import {
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const data = fighterService.getAll();
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const data = fighterService.search({ id });
+      if (!data) {
+        throw new Error(`Fighter wasn't found`);
+      }
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      const fighter = req.body;
+      const data = fighterService.add(fighter);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.patch(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const update = req.body;
+      const data = fighterService.update(id, update);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const data = fighterService.delete(id);
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };

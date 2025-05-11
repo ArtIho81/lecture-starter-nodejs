cde5d915619973ae161e2b3876ac33c95d44d807
Ihor Artomov <igora2406@gmail.com>
2025-05-11 22:13:21 +0300

    + user and fighter services

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,45 @@ import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
   // TODO: Implement methods to work with fighters
+  #defaultHealth = 85;
+
+  search(search) {
+    const item = fighterRepository.getOne(search);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  add(candidate) {
+    candidate.health = candidate.health ?? this.#defaultHealth;
+    candidate.name = candidate.name.toLowerCase();
+    const fighter = fighterRepository.create(candidate);
+    return fighter;
+  }
+
+  getAll() {
+    return fighterRepository.getAll();
+  }
+
+  update(id, update) {
+    if (update.name) {
+      update.name = update.name.toLowerCase();
+    }
+    const updatedFighter = fighterRepository.update(id, update);
+    if (!updatedFighter) {
+      throw new Error(`Fighter wasn't found`);
+    }
+    return updatedFighter;
+  }
+
+  delete(id) {
+    const deletedFighter = fighterRepository.delete(id);
+    if (deletedFighter.length === 0) {
+      throw new Error(`Fighter wasn't found`);
+    }
+    return deletedFighter;
+  }
 }
 
 const fighterService = new FighterService();
--- services/userService.js
+++ services/userService.js
@@ -10,6 +10,33 @@ class UserService {
     }
     return item;
   }
+
+
+  add(candidate) {
+    candidate.email = candidate.email.toLowerCase();
+    const user = userRepository.create(candidate);
+    return user;
+  }
+
+  getAll() {
+    return userRepository.getAll();
+  }
+
+  update(id, update) {
+    if (update.email) {
+      update.email = update.email.toLowerCase();
+    }
+    const updatedUser = userRepository.update(id, update);
+    return updatedUser;
+  }
+
+  delete(id) {
+    const deletedUser = userRepository.delete(id);
+    if (deletedUser.length === 0) {
+      throw new Error(`User wasn't found`);
+    }
+    return deletedUser;
+  }
 }
 
 const userService = new UserService();

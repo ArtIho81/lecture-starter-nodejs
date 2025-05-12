362d3788c13b47a07eda72815987e35652426504
Ihor Artomov <igora2406@gmail.com>
2025-05-12 18:57:06 +0300

    + response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,15 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
+  const errorData = (message) => ({ error: true, message });
+
+  if (res.err) {
+    const message = Array.isArray(res.err)
+      ? res.err.join(", ")
+      : res.err.message;
+    const status = message.match(`wasn't found`) ? 404 : 400;
+    return res.status(status).json(errorData(message));
+  }
+  res.status(200).json(res.data);
   next();
 };
 
